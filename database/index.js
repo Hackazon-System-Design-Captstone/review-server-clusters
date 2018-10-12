904888df6646cd8c3a698bf8196b8b7754efba46
Windsvept <junsubjjang@gmail.com>
2018-10-12 13:52:09 -0700

    west

--- database/index.js
+++ database/index.js
@@ -2,7 +2,7 @@ const pg = require('pg');
 
 const client = new pg.Client({
   user: 'jun',
-  host: 'ec2-18-144-26-84.us-west-1.compute.amazonaws.com',
+  host: 'ip-172-31-7-145.us-west-1.compute.internal',
   database: 'sdc',
   password: 'sd7763sd',
   port: 5432,
